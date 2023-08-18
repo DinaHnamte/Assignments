91e33f8804b8f2b9286ab36717b5a75184d1c93f
DinaHnamte <dinahnamte@gmail.com>
2023-08-18 23:31:33 +0530

    2nd

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  interface Form_Data {
+    name: string;
+    email: string;
+    phone: string;
+  }
+
+  const [form_data, set_form_data] = useState<Form_Data>({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    set_form_data((prev) => ({ ...prev, [name]: value }));
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="form">
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" onChange={handleChange} />
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" onChange={handleChange} />
+        <label htmlFor="phone">Phone</label>
+        <input type="text" name="phone" onChange={handleChange} />
+
+        <div>
+          <p>Name: {form_data.name}</p>
+          <p>Email: {form_data.email}</p>
+          <p>Phone: {form_data.phone}</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
